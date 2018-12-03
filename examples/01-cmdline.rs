3ca6d6affcd6f4d058340dbb34818198ef741a19
Mendelt Siebenga <msiebenga@gmail.com>
2018-12-03 13:46:30 +0100

    Parse input lines and call commands on context

--- examples/01-cmdline.rs
+++ examples/01-cmdline.rs
@@ -1,17 +1,32 @@
-use cmdr::{cmd_loop, Context};
+use cmdr::{cmd_loop, Context, Line};
 
 
 struct MyContext { }
+
+
+impl MyContext {
+    pub fn quit(&self, args: Vec<&str>) {
+        println!("quitterdequit!");
+    }
+}
 
 
 impl Context for MyContext {
     fn prompt(&self) -> String {
         "#".to_string()
     }
+
+    fn command(&mut self, line: Line) {
+        match line {
+            Line::Empty => self.empty(),
+            Line::Command("quit", args) => self.quit(args),
+            _ => self.default(line)
+        }
+    }
 }
 
 
 fn main(){
-    let context = MyContext {};
-    cmd_loop(&context);
+    let mut context = MyContext {};
+    cmd_loop(&mut context);
 }

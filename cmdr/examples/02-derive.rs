386d4a5713b18679be50fcfddd93450aa6d70a87
Mendelt Siebenga <msiebenga@gmail.com>
2019-01-03 17:14:58 +0100

    Create a cmdr macro to auto-call commands.

--- cmdr/examples/02-derive.rs
+++ cmdr/examples/02-derive.rs
@@ -1,10 +1,26 @@
-use cmdr::{ContextMacro};
+use cmdr::*;
 
-#[derive(ContextMacro)]
-struct MyContext { }
+struct ExampleScope { }
+
+#[cmdr]
+impl ExampleScope {
+    fn do_testcommand1(&self, args: Vec<&str>) -> CommandResult {
+        println!("command 1");
+        CommandResult::Succes
+    }
+
+    fn do_testcommand2(&self, args: Vec<&str>) -> CommandResult {
+        println!("command 2");
+        CommandResult::Succes
+    }
+
+    fn do_quit(&self, args: Vec<&str>) -> CommandResult {
+        CommandResult::Quit
+    }
+}
 
 
 fn main(){
-    let context = MyContext {};
-    context.do_stuff();
+    let mut scope = ExampleScope {};
+    cmd_loop(&mut scope);
 }

24554992e276b0ff2a56dfe3eaebb79be569dac5
Mendelt Siebenga <msiebenga@gmail.com>
2019-01-04 18:20:38 +0100

    Clean up
    Fixed warnings about unused results and parameters
    Cleaned up the examples and added comments

--- cmdr/examples/01-cmdline.rs
+++ cmdr/examples/01-cmdline.rs
@@ -1,23 +1,31 @@
+//! Example for the cmdr crate.
+//!
+//! Implements a simple cmdr application that does not use the cmdr macro.
+//! It provides two commands, One that gives a greeting to someone depending on the supplied
+//!   parameter and one to quit the application
+
 use cmdr::*;
 
+/// Example Cmdr scope
+struct GreeterScope { }
 
-struct ExampleScope { }
-
-
-impl ExampleScope {
+impl GreeterScope {
+    /// Cmdr command to greet someone.
     pub fn do_greet(&self, args: Vec<&str>) -> CommandResult {
         println!("Hello {}", args[0]);
         CommandResult::Succes
     }
 
-    pub fn do_quit(&self, args: Vec<&str>) -> CommandResult {
+    /// Cmdr command to quit the application by returning CommandResult::Quit
+    pub fn do_quit(&self, _args: Vec<&str>) -> CommandResult {
         println!("Quitting");
         CommandResult::Quit
     }
 }
 
-
-impl Scope for ExampleScope {
+/// Manual scope implementation for Cmdr. Normally you'd use the cmdr macro for this. Implements
+/// the command method that dispatches commands to functions implemented above.
+impl Scope for GreeterScope {
     fn command(&mut self, line: Line) -> CommandResult {
         match line {
             Line::Empty => self.empty(),
@@ -28,8 +36,8 @@ impl Scope for ExampleScope {
     }
 }
 
-
+/// Main function that creates the scope and starts a command loop for it
 fn main(){
-    let mut context = ExampleScope {};
-    cmd_loop(&mut context);
+    let mut scope = GreeterScope {};
+    cmd_loop(&mut scope);
 }

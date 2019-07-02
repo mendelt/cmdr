f68fd6f8ecb08ee3b668269f347146a343f9b556
Mendelt Siebenga <msiebenga@gmail.com>
2019-07-02 23:09:31 +0200

    Add initial version of translation example

---
+++ cmdr/examples/08-translate.rs
@@ -0,0 +1,29 @@
+//! Shows how to replace all strings that are output by cmdr with translated versions. This example
+//! provides Dutch translations for all messages built into cmdr.
+//!
+//! This translates;
+//! - Error messages by handling user_error
+//! -
+
+use cmdr::*;
+
+struct TranslatedScope {}
+
+#[cmdr]
+impl TranslatedScope {
+    /// Handle errors, output a translated error string for all known errors
+    fn handle_error(&mut self, error: CommandError) -> CommandResult {
+        match error {
+            CommandError::InvalidCommand { command } => {
+                println!("Onbekend commando: {}", command);
+                CommandResult::Ok
+            }
+            _ => CommandResult::Error(error),
+        }
+    }
+}
+
+fn main() {
+    cmd_loop(&mut TranslatedScope {});
+}
+

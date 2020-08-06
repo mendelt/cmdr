7c0c6f48a71847230c8ef2b1724e139d74f03117
Mendelt Siebenga <msiebenga@gmail.com>
2020-08-06 21:17:38 +0200

    refactor runner, remove box around reader

--- cmdr/src/runner.rs
+++ cmdr/src/runner.rs
@@ -7,7 +7,7 @@ use std::fmt::Debug;
 /// LineReader
 #[derive(Debug)]
 pub struct Runner<'a, S: Scope, R: LineReader> {
-    reader: Box<R>,
+    reader: R,
     scope: &'a mut S,
 }
 
@@ -15,17 +15,17 @@ impl<'a, S: Scope, R: LineReader> Runner<'a, S, R> {
     /// Create a new runner that takes lines from the `reader` and executes them using the `scope`
     pub fn new(reader: R, scope: &'a mut S) -> Self {
         Runner {
-            reader: Box::new(reader),
+            reader: reader,
             scope,
         }
     }
 
     /// Start reading lines and executing them
     pub fn run(&mut self) -> CommandResult {
-        let mut result = self.scope.run_lines(self.reader.as_mut());
+        let mut result = self.scope.run_lines(&mut self.reader);
 
         while let CommandResult::NewScope(scope_runner) = result {
-            result = scope_runner.run_lines(self.reader.as_mut());
+            result = scope_runner.run_lines(&mut self.reader);
         }
 
         result
